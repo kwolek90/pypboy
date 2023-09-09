7b03cfcd82f8003d7c10bb27d724f1fb8fcdbf07
kwolek90 <karolwolek@student.uw.edu.pl>
2023-09-09 22:20:25 +0200

    autoupdate on start

---
+++ prepare_env.sh
@@ -0,0 +1,12 @@
+#!/bin/sh
+
+sudo apt-get install python3 python3-pip python3-pygame vim git python3-numpy python3-pil
+git clone https://github.com/goodtft/LCD-show.git
+git clone https://github.com/kwolek90/pypboy.git
+pip install xmltodict requests
+mkdir -p ~/.config/lxsession/LXDE-pi
+cp /etc/xdg/lxsession/LXDE-pi/autostart ~/.config/lxsession/LXDE-pi
+echo "@python3 `pwd`/pypboy/main.py pi" >> ~/.config/lxsession/LXDE-pi
+cd LCD-show
+sudo chmod +x LCD35-show
+sudo ./LCD35-show
