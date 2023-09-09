9f159d157838211125c1a090760a8af812ae7b26
kwolek90 <karolwolek@student.uw.edu.pl>
2023-09-09 22:12:11 +0200

    autoupdate on start

--- launcher.sh
+++ launcher.sh
@@ -1,4 +1,14 @@
 #!/bin/sh
 
 cd /home/karol/pypboy
+
+wget -q --spider http://google.com
+
+if [ $? -eq 0 ]; then
+    echo "Online"
+    git pull
+else
+    echo "Offline"
+fi
+
 /usr/bin/python main.py pi
