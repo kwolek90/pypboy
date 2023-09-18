d2ee651973101b59c4834af2cfa98461f228dfb3
kwolek90 <karolwolek@student.uw.edu.pl>
2023-09-18 20:17:18 +0200

    turn on first dial

--- prepare_env.sh
+++ prepare_env.sh
@@ -8,6 +8,14 @@ pip install xmltodict requests pyky040
 mkdir -p ~/.config/lxsession/LXDE-pi
 cp /etc/xdg/lxsession/LXDE-pi/autostart ~/.config/lxsession/LXDE-pi
 echo "@bash `pwd`/pypboy/launcher.sh" >> ~/.config/lxsession/LXDE-pi/autostart
+PANEL_SETTINGS=~/.config/lxpanel/LXDE-pi/panels/panel
+cp /etc/xdg/lxpanel/LXDE-pi/panels/panel $PANEL_SETTINGS
+ENABLE_NOTIFICATIONS="notifications=1"
+DISABLE_NOTIFICATIONS="notifications=0"
+
+# disable notifications
+sudo sed -i -e "s/$ENABLE_NOTIFICATIONS/$DISABLE_NOTIFICATIONS/g" $PANEL_SETTINGS
+
 cp `pwd`/pypboy/launcher.sh run
 cd LCD-show
 sudo chmod +x LCD35-show
