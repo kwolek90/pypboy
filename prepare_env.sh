65f1ca7d3cad01c07fb3a4c2dc94becf3370b9c1
kwolek90 <karolwolek@student.uw.edu.pl>
2025-03-07 23:31:11 +0100

    Env update

--- prepare_env.sh
+++ prepare_env.sh
@@ -1,11 +1,11 @@
 #!/bin/sh
 
 sudo apt-get update
-sudo apt-get -y install python python-pip python-pygame vim git python-numpy python-pil onboard
-pip install xmltodict requests
+sudo apt-get -y install python3 python3-pip python3-pygame vim python3-numpy python3-pil onboard
+
 mkdir -p ~/.config/lxsession/LXDE-pi
 cp /etc/xdg/lxsession/LXDE-pi/autostart ~/.config/lxsession/LXDE-pi
-echo "@bash `pwd`/pypboy/launcher.sh" >> ~/.config/lxsession/LXDE-pi/autostart
+echo "@bash `pwd`/launcher.sh" >> ~/.config/lxsession/LXDE-pi/autostart
 PANEL_SETTINGS=~/.config/lxpanel/LXDE-pi/panels/panel
 cp /etc/xdg/lxpanel/LXDE-pi/panels/panel $PANEL_SETTINGS
 ENABLE_NOTIFICATIONS="notifications=1"
@@ -14,6 +14,5 @@ DISABLE_NOTIFICATIONS="notifications=0"
 # disable notifications
 sudo sed -i -e "s/$ENABLE_NOTIFICATIONS/$DISABLE_NOTIFICATIONS/g" $PANEL_SETTINGS
 
-cp `pwd`/pypboy/launcher.sh run
-cd ~/pypboy
-venv/bin/python -m pip install -r requirements.txt
+python3 -m venv venv
+venv/bin/python3 -m pip install -r requirements.txt
